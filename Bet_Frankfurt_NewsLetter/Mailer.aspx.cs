87235dfb881e8c45e586528cceb49ad561c331ae
yuvaloni <yuvon2@gmail.com>
2014-03-13 17:36:58 +0200

    RemoveMe

--- Bet_Frankfurt_NewsLetter/Mailer.aspx.cs
+++ Bet_Frankfurt_NewsLetter/Mailer.aspx.cs
@@ -61,6 +61,8 @@ namespace Bet_Frankfurt_NewsLetter
             {
                 var toAddress = new MailAddress(r3.GetString(3));
                 string msg = "<html Content-Type: text/html> <body>";
+                msg += @" </br> זוהי הודעה אוטומטית";
+                msg += @"<a href='http://betfrankufrtnewsletter.apphb.com\/removeme?user=" + r3.GetString(3) + "'>אם אתה מעוניין להפסיק לקבל הודעות אלו לחץ כאן</a>";
                 if (r3.GetInt32(5) == 1)
                     msg += children;
                 msg += "</br>";
---
+++ Bet_Frankfurt_NewsLetter/RemoveMe.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+
+namespace Bet_Frankfurt_NewsLetter
+{
+    public partial class RemoveMe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OleDbConnection Con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='" + Path.Combine(Server.MapPath("~"), "Frankfurt.mdb") + "'");
+            Con.Open();
+            OleDbCommand Com = new OleDbCommand("DELETE * FROM Contacts WHERE [email] = @n", Con);
+            Com.Parameters.Add(new OleDbParameter("@n", OleDbType.WChar)).Value = Request.QueryString["user"];
+            Com.ExecuteNonQuery();
+            Con.Close();
+            Response.Write("הוסרת בהצלחה מרשימת התפוצה");
+        }
+    }
+}
